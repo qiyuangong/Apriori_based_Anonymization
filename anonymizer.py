c4d266230fd861baef516e76485c80c2d4a7296f
qiyuangong <qiyuangong@gmail.com>
2014-04-15 17:12:14 +0800

    Important Update:Add another kind of GH for condition Remove useless fucitons and files: remove treefile_marry remove treefile_income remove treefile_DOBMM remove treefile_DOBYY remove treefile_EDUCYEAR remove treefile_SEX remove treefile_RACEX
    Modify maketree.py
    Add even distribution tree make fuction gen_even_tree
    This fuction divide generalization hierarchies by fan-out.
    Default fanout = 5, for small data set fanout = 4
    
    Modify read_data.py
    Remove useless reading part for QID, keep SA remain.

--- anonymizer.py
+++ anonymizer.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 #coding=utf-8
 from Apriori_based_Anon import Apriori_based_Anon
-from read_data import readdata, readtree
+from read_data import read_data, read_tree
 # Poulis set k=25, m=2 as default!
 
 if __name__ == '__main__':
     #read gentree tax
-    att_tree = readtree()
+    att_tree = read_tree()
     #read record
-    data = readdata()
+    trans = read_data()
     # init AA
-    aa = Apriori_based_Anon(att_tree[-1])
-    trans = [row[-1] for row in data[:200]]
+    aa = Apriori_based_Anon(att_tree)
     # remove duplicate items
     for i in range(len(trans)):
         trans[i] = list(set(trans[i]))
     # cut = aa.DA(trans)
-    cut = aa.AA(trans)
+    cut = aa.AA(trans[:])
     print "Final Cut"
     print cut
     result = aa.trans_gen(trans, cut)
