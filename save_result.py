f2763ede82bdd4a5e311881e853fb3fe04c6ace5
qiyuangong <qiyuangong@gmail.com>
2014-06-09 17:04:54 +0800

    Update save_result.py
    Add pickle modual as default. Pickle is a good solution for keeping
    python data.

--- save_result.py
+++ save_result.py
@@ -4,19 +4,26 @@
 # store
 
 import socket
+import pickle
 from datetime import datetime
 from ftp_upload import ftp_upload
 
-def save_to_file(result):
+def save_to_file(result, flag=0):
     print "Saving result...."
     hostname = socket.gethostname()
     file_tail = datetime.now().strftime('%Y-%m-%d-%H') + '.txt'
     file_path = 'output/'
     file_name = hostname + '-result' + file_tail
-    file_result = open(file_path + file_name,'w')
-    for record in result:
-        line = ';'.join(record) + '\n'
-        file_result.write(line)
+    if flag:
+        # write file in text
+        file_result = open(file_path + file_name,'w')
+        for record in result:
+            line = ';'.join(record) + '\n'
+            file_result.write(line)
+    else:
+        # write file using pickle
+        file_result = open(file_path + file_name,'wb')
+        pickle.dump(result, file_result)
     file_result.close()
     try:
         ftp_upload(file_name, file_path)
